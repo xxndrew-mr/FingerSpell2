12373117fc204c395c71c383e37c4bbb06f3898f
Ramadhani Putra <putrapangestu3355@gmail.com>
2024-06-13 21:48:11 +0700

    penambahan untuk login

---
+++ app/src/main/java/com/example/fingerspell/data/pref/UserRepository.kt
@@ -0,0 +1,32 @@
+package com.example.fingerspell.data.pref
+
+import kotlinx.coroutines.flow.Flow
+
+class UserRepository private constructor(
+    private val userPreference: UserPreference
+) {
+
+    suspend fun saveSession(user: UserModel) {
+        userPreference.saveSession(user)
+    }
+
+    fun getSession(): Flow<UserModel> {
+        return userPreference.getSession()
+    }
+
+    suspend fun logout() {
+        userPreference.logout()
+    }
+
+    companion object {
+        @Volatile
+        private var instance: UserRepository? = null
+        fun getInstance(
+            apiService: ApiService,
+            userPreference: UserPreference
+        ): UserRepository =
+            instance ?: synchronized(this) {
+                instance ?: UserRepository(userPreference)
+            }.also { instance = it }
+    }
+}
